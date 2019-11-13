7b68c56f8fe5efe163f156a303adb25b36b79465
Myron <dubtwee@gmail.com>
2019-11-12 21:50:38 -0600

    adding the code for the next step of adding a service charge to the masterticket application code

--- python-basics/masterticket.py
+++ python-basics/masterticket.py
@@ -53,24 +53,60 @@ tickets_remaining = 100
 # print("Sorry the tickets are all sold all sold out :(")
 
 # ----------------------------------------------------------------------
-# REMOVING COMMENTS TO STREAMLINE CODE
+# MAKING CODE LOOP UNTIL ALL TICKETS ARE SOLD
 # ----------------------------------------------------------------------
+
+# while tickets_remaining > 0:
+#     print("There are {} tickets remaining.".format(tickets_remaining))
+#     name = input("What is your name? ")
+#     num_tickets = input("How many tickets would you like, {}? ".format(name))
+#     # Expect a ValueError to happen and handle it appropriately...
+#     try:
+#         num_tickets = int(num_tickets)
+#         # Raise a ValueError if the requested tickets are more than the available tickets
+#         if num_tickets > tickets_remaining:
+#             raise ValueError("There are only {} tickets remaining. ".format(tickets_remaining))
+#     except ValueError as err:
+#         # Include error text in the output
+#         print("Oh no, we ran into an issue. {} Please try again. ".format(err))
+#     else:
+#         amount_due = num_tickets * TICKET_PRICE
+#         print("The total due is ${}".format(amount_due))
+#         will_purchase = input("{} would you like to purchase these tickets (Y/N)? ".format(name))
+#         if will_purchase.lower() == "y":
+#             # TODO: Gather credit card information and process it.
+#             print("SOLD!")
+#             tickets_remaining -= num_tickets
+#         else:
+#             print("Thank you anyways, {}.".format(name))
+
+# print("Sorry the tickets are all sold all sold out :(")
+
+# ----------------------------------------------------------------------
+# ADDING SERVICE CHARGES TO TICKET PURCHASE
+# ----------------------------------------------------------------------
+
+# Create a new constant for the 2 dollar service charge
+SERVICE_CHARGE = 2
+
+# Create calculate_price function
+# - it takes number of tickets and returns num_tickets * TICKET_PRICE
+def calculate_price(number_of_tickets):
+    # Add the service charge to the result
+    return (number_of_tickets * TICKET_PRICE) + SERVICE_CHARGE
 
 while tickets_remaining > 0:
     print("There are {} tickets remaining.".format(tickets_remaining))
     name = input("What is your name? ")
     num_tickets = input("How many tickets would you like, {}? ".format(name))
-    # Expect a ValueError to happen and handle it appropriately...
     try:
         num_tickets = int(num_tickets)
-        # Raise a ValueError if the requested tickets are more than the available tickets
         if num_tickets > tickets_remaining:
             raise ValueError("There are only {} tickets remaining. ".format(tickets_remaining))
     except ValueError as err:
-        # Include error text in the output
         print("Oh no, we ran into an issue. {} Please try again. ".format(err))
     else:
-        amount_due = num_tickets * TICKET_PRICE
+        amount_due = calculate_price(num_tickets)
         print("The total due is ${}".format(amount_due))
         will_purchase = input("{} would you like to purchase these tickets (Y/N)? ".format(name))
         if will_purchase.lower() == "y":

904a5cd554b3bded2a9001e801cb181405f004b2
Myron <dubtwee@gmail.com>
2019-11-26 23:09:44 -0600

    adding unpacking code to the packing exercise

--- python-dictionaries/packing.py
+++ python-dictionaries/packing.py
@@ -19,3 +19,25 @@ def print_teacher2(**kwargs):
 print_teacher2(name='Ashley', job='Teacher', topic='Python')
 print('----------')
 print_teacher2(name='Ashley', job='Teacher', topic='Python', second_topic='JavaScript')
+
+print('\n==========')
+print('UNPACKING')
+print('==========\n')
+
+# CONSIDER THIS CODE:
+# dictionary holds 3 key:value pairs
+teacher = {
+  'name':'Ashley',
+  'job':'Instructor',
+  'topic':'Python'
+}
+
+def print_teacher3(name, job, topic):
+    print(name)
+    print(job)
+    print(topic)
+    
+# in order to pass all of the key:value pairs to the
+# print_teacher3 function we need to unpack the dictionary
+print_teacher3(**teacher)
+# its the ** that unpacks our dictionaries key:value pairs into three keywordarguments
